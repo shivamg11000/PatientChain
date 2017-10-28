a088f11e2b1dc7deee0e7b56602e03bc0fe69481
SHIVAM GUPTA <shivamg11000@gmail.com>
2017-10-28 05:48:32 +0530

    update

--- src/js/index.js
+++ src/js/index.js
@@ -7,15 +7,24 @@ $(document).ready(function(){
         mobileFirst: true
     });
 
+
     $("#patient").click(function(){
         $("#doctor-login").css("display", "none")
         $("#patient-login").css('display', 'block')
+        $(".login-page #doctor").css('background-color','#47EBB2')
+        $(".login-page #patient").css('background-color', '#00D393')
     })
+
+    
 
     $("#doctor").click(function(){
         $("#doctor-login").css("display", "block")
         $("#patient-login").css('display', 'none')
+        $(".login-page #doctor").css('background-color', '#00D393')
+        $(".login-page #patient").css('background-color', '#47F8BF')
     })
+
+
 
   });
 
